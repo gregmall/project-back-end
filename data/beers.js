96a9a93c718a8d1a97e3eb9dc9257d255b8812a9
Greg Mall <gregmall157@gmail.com>
2020-08-12 16:05:18 -0700

    first commit

---
+++ data/beers.js
@@ -0,0 +1,31 @@
+module.exports = [
+  {
+    
+    name: 'Pabst',
+    domestic: true,
+    category: 'lager',
+    price: 2,
+  },
+  {
+    name: 'Carlsburg',
+    domestic: false,
+    category: 'pilsner',
+    price: 5,
+  },
+  {
+    name: 'Sierra Nevada',
+    domestic: true,
+    category: 'micro brew',
+    price: 4,
+  },
+  {
+    name: 'Modelo',
+    domestic: false,
+    category: 'lager',
+    price: 3,
+
+  }
+
+
+
+];
